3977723ece5572cb25d974b7b8ccd3018e3da971
Rotich <104131298+Tetioo@users.noreply.github.com>
2023-07-17 20:48:38 +0300

    Delete connection.php

--- connection.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$conn = mysqli_connect('localhost', 'id20947709_csproject1', '@209QB6OC)q~qS<C', 'id20947709_nourishkids');
-if (!$conn){
-    die("Could not connect".mysqli_connect_error());
-}
-echo "Connected successfully";
-?>
