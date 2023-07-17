6ddc6a6e891fc17cb075608fb4ad4a8ab53ec689
Rotich <104131298+Tetioo@users.noreply.github.com>
2023-07-17 20:48:35 +0300

    Create compformprocess.php

---
+++ compformprocess.php
@@ -0,0 +1,21 @@
+<?php
+
+include 'config.php';
+
+$number = count($_POST["name"]);
+if($number > 1 )
+
+{
+    for ($i=0; $i<$number; $i++)
+    {
+
+        if(trim($_POST["namee"]));
+    }
+    
+}
+else
+{
+   echo "Enter Name";
+}
+
+?>
