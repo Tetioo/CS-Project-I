0d006505007ca95636d6415c2faae2329e43c976
Rotich <104131298+Tetioo@users.noreply.github.com>
2023-07-17 20:01:15 +0300

    Create create-newpass.php

---
+++ login_register/create-newpass.php
@@ -0,0 +1,22 @@
+
+<main>
+<div class="wrapper-main">
+    <section class="section-default">
+
+
+
+<form action= "reset-password.php" method="post">
+   
+    <input type="password" name="password" placeholder="Enter new password">
+    <input type="password" name="pwd-repeat" placeholder="Repeat new password">
+    <button type="submit" name="reset-password-submit">Reset password</button>
+    </form>
+
+
+
+
+</section>
+</div>
+
+</main>
+
