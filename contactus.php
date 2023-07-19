a7034598e52b40a85244b50583ea9c0f0e9e9873
Rotich <104131298+Tetioo@users.noreply.github.com>
2023-07-19 10:56:31 +0300

    Create contactus.php

---
+++ contactus.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="dist/css/adminlte.min.css">
+    <title>Contact Us Form In Php</title>
+</head>
+<body>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 m-auto">
+                <div class="card mt-5">
+                    <div class="card-title">
+                        <h2 class="text-center py-2"> Contact Us </h2>
+                        <hr>
+                        <?php 
+                            $Msg = "";
+                            if(isset($_GET['error']))
+                            {
+                                $Msg = " Please Fill in the Blanks ";
+                                echo '<div class="alert alert-danger">'.$Msg.'</div>';
+                            }
+                            if(isset($_GET['success']))
+                            {
+                                $Msg = " Your Message Has Been Sent ";
+                                echo '<div class="alert alert-success">'.$Msg.'</div>';
+                            }
+                        
+                        ?>
+                    </div>
+                    <div class="card-body">
+                    <form action="contact-form.php" method="POST">
+        <div class="input-group mb-3">
+          <input type="text"  name="name"  class="form-control" placeholder="Organization name">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-envelope"></span>
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input type="text" name="mail"  class="form-control" placeholder="Email">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input type="text" name="subject"  class="form-control" placeholder="Subject">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <textarea name="message" placeholder="Message"></textarea>
+        </div>
+       
+          <!-- /.col -->
+          <div class="col-4">
+            <button type="submit" name = "submit"  class="btn btn-primary btn-block">SEND MAIL</button>
+          </div>
+          <!-- /.col -->
+        </div>
+      </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
